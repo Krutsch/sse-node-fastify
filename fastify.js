e3a0ecc66c9541da4b122a4f95b87f48992508f4
Fabian Krutsch <f.krutsch@gmx.de>
2020-07-19 22:30:28 +0100

    update to make it work - add headers to reply.raw

--- fastify.js
+++ fastify.js
@@ -6,19 +6,17 @@ fastify.register(require("fastify-static"), {
 });
 
 fastify.get("/events", (_req, reply) => {
-  reply
-    .type("text/event-stream")
-    .header("Cache-Control", "no-cache")
-    .header("Connection", "keep-alive")
-    .code(200);
+  reply.raw.setHeader("Content-Type", "text/event-stream");
+  reply.raw.setHeader("Cache-Control", "no-cache");
+  reply.raw.setHeader("Connection", "keep-alive");
 
   const timeout = 500;
   setInterval(() => {
     const id = Date.now();
     const data = `Hello World ${id}`;
     const message = `retry: ${timeout}\nid:${id}\ndata: ${data}\n\n`;
-    reply.send(message); // definitely more than 500ms
-    // reply.raw.write(message); // this is not working
+    // reply.send(message); // do not use this method
+    reply.raw.write(message);
   }, timeout);
 });
 
